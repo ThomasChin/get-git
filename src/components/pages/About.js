f4c74e29a1a2abe95b1500e008405d7e87bcf6da
ThomasChin <tchin10@outlook.com>
2019-07-01 15:27:42 -0700

    Add React Router and About page

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About This App</h1>
+      <p>Personal project for gathering info from the GitHub Developers API</p>
+      <p>Build using React</p>
+      <p>Version: 1.0.0 </p>
+    </Fragment>
+  );
+};
+
+export default About;
